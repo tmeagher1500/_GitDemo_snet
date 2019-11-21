898dde5e01257e4038c254bd2709f3954cc61b25
tmeagher <tmeagher1500@gmail.com>
2019-11-21 03:24:46 -0500

    First commit ...

---
+++ src/__GitDemo_sdet/Test.java
@@ -0,0 +1,17 @@
+package __GitDemo_sdet;
+
+public class Test {
+
+	public static void main(String[] args) {
+
+		String hungry = "yes";
+		
+		if (hungry == "yes") 
+			System.out.println("eat something");
+		else {
+			System.out.println("Do work ...");
+		}
+
+	}
+
+}
